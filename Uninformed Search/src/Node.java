07f119bbc2e9251e42b71327802f13fc271bc8f8
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-06-04 09:19:24 -0400

    a

--- Uninformed Search/src/Node.java
+++ Uninformed Search/src/Node.java
@@ -8,35 +8,60 @@ public class Node {
 
 	public String Map; //test map of the United States
 	
-		Node left;
-		Node right;
+		int posx;
+		int posy;
 		
-	public Node (String Map, Node first, Node second)
+		ArrayList <Node> children;
+		Node parent;
+		
+	public Node (String Map, int x, int posx, int posy)
 	{
 		this.Map = Map;
-		this.left.left = first;
-		this.right = second;
+		this.posx = posx;
+		this.posy = posy;
 	}
 	
-	 public ArrayList<Node>getChildren() //return a list of all connected nodes
+	 public Node(String string, Object object) 
+	 {
+		 
+	 }
+
+	public ArrayList<Node>getChildren() //return a list of all connected nodes
 	 {
 	        ArrayList<Node>childNodes = new ArrayList<>();
 	        
-	        if (this.left != null)
+	        if (this.x != null)
 	        {
-	            childNodes.add(left);
+	            childNodes.add(x);
 	        }
 	        
-	        if (this.right != null) 
+	        if (this.y != null) 
 	        {
-	            childNodes.add(right);
+	            childNodes.add(y);
 	        }
 	        return childNodes;
 	    }
 	 
+	 public class setChildren(ArrayList<Node>children)
+	 {
+		 this.children = children;
+	 }
+	 
+	 public boolean equals(Object x)
+	 {
+		 Node check = (Node)x;
+		 return(check.point.equals(x));
+	 }
 	 
 	 public boolean removeChild(Node n)
 	 {
 	        return false;
 	 }
 }
+
+	public void setChildren(ArrayList<Node> arrayList) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	}
